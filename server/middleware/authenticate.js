54915f7adce688e88dc2801accb5abafa11cb105
Alen Redek <alen@redek.me>
2017-05-03 13:59:07 +0200

    Add GET /users/me and authentication middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,19 @@
+var {User} = require('./../models/user');
+
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => { // Model method
+    if (!user) {
+      return Promise.reject(); // Skips the code below and goes directly to catch block
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  }).catch((e) => {
+    res.status(401).send();
+  });
+};
+
+module.exports = {authenticate};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID}    = require('mongodb');
 var {mongoose}      = require('./db/mongoose');
 var {Todo}          = require('./models/todo');
 var {User}          = require('./models/user');
+var {authenticate}  = require('./middleware/authenticate');
 
 var app = express();
 
@@ -109,8 +110,6 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  //User.findByToken(); // Model method
-
   user.save().then(() => {
     return user.generateAuthToken(); // Instance method
   }).then((token) => {
@@ -118,6 +117,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
